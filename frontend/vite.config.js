a75b60ff3116ae09f8eefd82e9055b56990a27ee
d3skdevelopers <d3skdevelopers@gmail.com>
2026-02-16 20:28:50 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  server: {
+    port: 5173,
+    open: true
   }
 })
